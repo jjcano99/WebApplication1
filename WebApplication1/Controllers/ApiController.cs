386eacc654f5d9eb48225820be3fe146a9abbadb
JJCT <Usuario@DESKTOP-UIND824>
2018-08-27 19:47:16 +0200

    Add project files.

---
+++ WebApplication1/Controllers/ApiController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("[controller]")]
+    public class ApiController : Controller
+    {
+        // GET: api/<controller>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/<controller>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+        // GET api/prueba1
+        [HttpGet("prueba1")]
+        public string Prueba1()
+        {
+            return "Hola Mundo1";
+        }
+
+        // GET api/prueba2
+        [HttpGet("prueba2")]
+        public string Prueba2()
+        {
+            return "Hola Mundo2";
+        }
+
+        // GET api/prueba3
+        [HttpGet("prueba3")]
+        public string Prueba3()
+        {
+            return "Hola Mundo3";
+        }
+
+
+
+
+        // POST api/<controller>
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        [HttpPost("prueba4")]
+        public JsonResult Post()
+        {
+            return Json( "Hola Mundo4");
+        }
+
+
+
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication2/Controllers/Api2Controller.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("[controller]")]
+    [ApiController]
+   // public class Api2Controller : ControllerBase
+ public class Api2Controller : Controller
+    {
+
+        [HttpGet("prueba1")]
+        public string Prueba1()
+        {
+            return "Hola Mundo1";
+        }
+
+        // GET api2/prueba2
+        [HttpGet("prueba2")]
+        public string Prueba2()
+        {
+            return "Hola Mundo2";
+        }
+
+        // GET api2/prueba3
+        [HttpGet("prueba3")]
+        public string Prueba3()
+        {
+            return "Hola Mundo3";
+        }
+
+
+
+
+        // POST api2/<controller>
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        [HttpPost("prueba4")]
+        public JsonResult Post()
+        {
+            return Json("Hola Mundo4 con CORS");
+        }
+
+        [HttpPost("prueba5")]
+        public JsonResult Prueba5(Persona persona)
+        {
+            var result = $"Hola Mundo4 con CORS: {persona.nombre} {persona.apellidos} ";
+            return Json(result);
+
+        }
+
+        [HttpPost("prueba6")]
+        public string Prueba6(Persona persona)
+        {
+            var result = $"Hola Mundo4 con CORS: {persona.nombre} {persona.apellidos} ";
+            return result;
+
+        }
+
+
+
+        //// GET api/values
+        //[HttpGet]
+        //public ActionResult<IEnumerable<string>> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public ActionResult<string> Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
